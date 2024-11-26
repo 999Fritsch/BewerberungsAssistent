58bf0ae7f92b15f4b4597927a9189e92d33b0ad6
GabrielHuettenberger <gabriel@huettenberger.de>
2024-11-26 10:37:16 +0000

    ein paar initiale funktionen

--- database_scripts/initialize_db.py
+++ database_scripts/initialize_db.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-connection = sqlite3.connect("data/assessment.db")
+connection = sqlite3.connect("./data/assessment.db")
 connection.execute("PRAGMA foreign_keys = 1")
 
 cursor = connection.cursor()
@@ -9,7 +9,9 @@ cursor.execute("CREATE TABLE IF NOT EXISTS question (id INTEGER PRIMARY KEY, ski
 cursor.execute("CREATE TABLE IF NOT EXISTS skill (id INTEGER PRIMARY KEY, name)")
 cursor.execute("CREATE TABLE IF NOT EXISTS applicant (id INTEGER PRIMARY KEY, score)")
 cursor.execute("CREATE TABLE IF NOT EXISTS skillset (id INTEGER PRIMARY KEY, skill, grade, FOREIGN KEY (skill) REFERENCES skill (id))")
-cursor.execute("CREATE TABLE IF NOT EXISTS position (id INTEGER PRIMARY KEY, name, description, skillset, FOREIGN KEY (skillset) REFERENCES skillset (id))")
+cursor.execute("CREATE TABLE IF NOT EXISTS position (id INTEGER PRIMARY KEY, name, description TEXT, skillset, FOREIGN KEY (skillset) REFERENCES skillset (id))")
 
-result = cursor.execute("SELECT name FROM sqlite_master")
-print(result.fetchall())
+cursor.execute("INSERT INTO position (id, name, description) VALUES (1, '"'testposition'"', '"'Dies ist eine Testjobbeschreibung'"')")
+connection.commit()
+
+
